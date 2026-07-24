bab0c3f366120a37d45490bf6e8cdfe52be29e1e
Yuli Zhang Hung <144866408+yuzhanghung@users.noreply.github.com>
2026-07-23 20:35:13 -0700

    Add: unionFind - submission-0

---
+++ Data Structures & Algorithms/unionFind/submission-0.py
@@ -0,0 +1,34 @@
+class UnionFind:
+    
+    def __init__(self, n: int):
+        self.parent = [i for i in range(n)]
+        self.size = [1] * n
+        self.components = n
+
+    def find(self, x: int) -> int:
+        if x not in self.parent:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+        
+    def isSameComponent(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+
+    def union(self, x: int, y: int) -> bool:
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x != root_y:
+            if self.size[root_x] < self.size[root_y]:
+                self.parent[root_x] = root_y
+                self.size[root_y] += self.size[root_x]
+            else:
+                self.parent[root_y] = root_x
+                self.size[root_x] += self.size[root_y]
+            self.components -= 1
+            return True
+        return False
+        
+
+    def getNumComponents(self) -> int:
+        return self.components
