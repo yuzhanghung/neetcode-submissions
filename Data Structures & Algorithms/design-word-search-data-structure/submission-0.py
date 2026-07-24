6cbe86ab12192f4975eb0cc9b1f016a491295289
Yuli Zhang Hung <144866408+yuzhanghung@users.noreply.github.com>
2026-07-23 19:52:15 -0700

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,23 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.words = []
+        
+
+    def addWord(self, word: str) -> None:
+        self.words.append(word)
+
+    def search(self, word: str) -> bool:
+        for w in self.words:
+            if len(w) != len(word):
+                continue
+            i = 0
+            while i < len(w):
+                if w[i] == word[i] or word[i] == '.':
+                    i += 1
+                else:
+                    break
+            if i == len(w):
+                return True
+
+        return False
