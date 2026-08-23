7f2f257c22d0659a9f6e1e631c233000e65eaaab
Yuli Zhang Hung <144866408+yuzhanghung@users.noreply.github.com>
2026-08-23 09:52:16 -0700

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        n = len(nums)
+        for i in range(n + 1): 
+            if i not in num_set:
+                return i
