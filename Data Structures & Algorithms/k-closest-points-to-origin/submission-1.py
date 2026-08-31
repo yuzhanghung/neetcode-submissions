81f1ec676ed6826a6e692178f7aa6fc61489876b
Yuli Zhang Hung <144866408+yuzhanghung@users.noreply.github.com>
2026-08-31 09:17:04 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        euclidean = lambda x: x[0] ** 2 + x[1] ** 2
+
+        def partition(l, r):
+            pivotIdx = r
+            pivotDist = euclidean(points[pivotIdx])
+            i = l
+            for j in range(l, r):
+                if euclidean(points[j]) <= pivotDist:
+                    points[i], points[j] = points[j], points[i]
+                    i += 1
+            points[i], points[r] = points[r], points[i]
+            return i
+
+        L, R = 0, len(points) - 1
+        pivot = len(points)
+
+        while pivot != k:
+            pivot = partition(L, R)
+            if pivot < k:
+                L = pivot + 1
+            else:
+                R = pivot - 1
+        return points[:k]
