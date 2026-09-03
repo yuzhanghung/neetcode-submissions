33cab504bdddb9da4bc63ff405e27e92bd09aa7f
Yuli Zhang Hung <144866408+yuzhanghung@users.noreply.github.com>
2026-09-03 08:06:17 -0700

    Add: graph - submission-1

---
+++ Data Structures & Algorithms/graph/submission-1.py
@@ -0,0 +1,34 @@
+class Graph:
+    
+    def __init__(self):
+        self.adj_list = {}
+
+    def addEdge(self, src: int, dst: int) -> None:
+        if src not in self.adj_list:
+            self.adj_list[src] = set()
+        if dst not in self.adj_list:
+            self.adj_list[dst] = set()
+        self.adj_list[src].add(dst)
+
+    def removeEdge(self, src: int, dst: int) -> bool:
+        if src not in self.adj_list or dst not in self.adj_list[src]:
+            return False
+        self.adj_list[src].remove(dst)
+        return True
+
+
+    def hasPath(self, src: int, dst: int) -> bool:
+        visited = set()
+        return self._dfs(src, dst, visited)
+
+    def _dfs(self, src: int, dst: int, visited: set) -> bool:
+        if src == dst:
+            return True
+        visited.add(src)
+        for neighbor in self.adj_list.get(src, []):
+            if neighbor not in visited:
+                if self._dfs(neighbor, dst, visited):
+                    return True
+
+        return False
+
