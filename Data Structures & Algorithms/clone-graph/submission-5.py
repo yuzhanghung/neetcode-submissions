66b6f16af3523b66b7ae85dade2d71d04cef0322
Yuli Zhang Hung <144866408+yuzhanghung@users.noreply.github.com>
2026-09-24 14:48:51 -0700

    Add: clone-graph - submission-5

---
+++ Data Structures & Algorithms/clone-graph/submission-5.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        old_to_new = {}
+
+        def dfs(cur):
+            if cur in old_to_new:
+                return old_to_new[cur]
+            
+            copy = Node(cur.val)
+            old_to_new[cur] = copy
+
+            for nei in cur.neighbors:
+                old_to_new[cur].neighbors.append(dfs(nei))
+
+            return copy
+
+        return dfs(node) if node else None
+
+        
